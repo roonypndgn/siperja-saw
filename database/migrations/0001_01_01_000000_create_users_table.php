242d702648e356be12992a4fcd057fc617fa9f2b
roonypndgn <pandianganronny@gmail.com>
2026-04-30 15:04:49 +0700

    membuat field pada migrasi user dan membuat model user

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -16,7 +16,10 @@ return new class extends Migration
             $table->string('name');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
+            $table->string('nip')->unique();
             $table->string('password');
+            $table->string('foto')->nullable();
+            $table->enum('role', ['admin','petugas'])->default('petugas');
             $table->rememberToken();
             $table->timestamps();
         });
